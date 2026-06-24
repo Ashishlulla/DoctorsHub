76070031abcb3dbe77500569686d2d729c5b725b
Ashish Lulla <lullaashish2807@gmail.com>
2026-06-24 20:29:17 +0530

    Initial ASP.NET Core MVC project setup

---
+++ DoctorsHub/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DoctorsHub.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
