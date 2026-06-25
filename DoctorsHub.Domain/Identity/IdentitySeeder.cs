cc3d476d47955455566ed5e10095d87e30e6d88c
Ashish Lulla <lullaashish2807@gmail.com>
2026-06-25 14:59:57 +0530

    feat(identity): seed roles and admin user

---
+++ DoctorsHub.Domain/Identity/IdentitySeeder.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoctorsHub.Domain.Identity
+{
+    public static class IdentitySeeder
+    {
+        public async static Task SeedRolesAndAdminAsync(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager) 
+        {
+            string[] roles = { "Patient", "Doctor", "Admin"};
+
+            foreach (var role in roles) 
+            {
+                if (! await roleManager.RoleExistsAsync(role))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+
+            //create Admin User
+            var adminEmail = "admin@DoctorsHub.com";
+
+            var adminUser = await userManager.FindByEmailAsync(adminEmail);
+
+            if (adminUser == null) 
+            {
+                adminUser = new ApplicationUser()
+                {
+                    UserName = adminEmail,
+                    Email = adminEmail,
+                    EmailConfirmed = true
+                };
+                var result = await userManager.CreateAsync(adminUser, "Admin123");
+                if (result.Succeeded) 
+                {
+                    await userManager.AddToRoleAsync(adminUser, "Admin");
+                }
+            }
+        }
+    }
+}
--- DoctorsHub/Program.cs
+++ DoctorsHub/Program.cs
@@ -1,4 +1,6 @@
+using DoctorsHub.Domain.Identity;
 using DoctorsHub.Web.Configurations;
+using Microsoft.AspNetCore.Identity;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,17 +14,28 @@ builder.Services.AddIdentityService();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+
+//Add Identity Scope
+using (var scope = app.Services.CreateScope()) 
 {
-    app.UseMigrationsEndPoint();
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+    var useManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+
+    await IdentitySeeder.SeedRolesAndAdminAsync(roleManager, useManager);
 }
-else
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseMigrationsEndPoint();
+    }
+    else
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
 
 app.UseHttpsRedirection();
 app.UseRouting();
