cb536eeecb17ef2ba961d362234fd6b32fb72b44
Ashish Lulla <lullaashish2807@gmail.com>
2026-07-01 00:21:59 +0530

    Add Appointment repository with CRUD operations

---
+++ DoctorHub.Application/Interfaces/RepositoryContracts/IAppointmentRepository.cs
@@ -0,0 +1,13 @@
+﻿using DoctorsHub.Domain.Entities;
+
+namespace DoctorsHub.Application.Interfaces.RepositoryContracts
+{
+    public interface IAppointmentRepository 
+    {
+        Task AddAsync(Appointment appointment);
+        Task<List<Appointment>> GetAppointmentsAsync();
+        Task<Appointment> GetByIdAsync(int id);
+        Task UpdateAsync(Appointment appointment);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ DoctorsHub.Infrastructure/Repositories/AppointmentRepository.cs
@@ -0,0 +1,55 @@
+﻿using DoctorsHub.Application.Interfaces.RepositoryContracts;
+using DoctorsHub.Domain.Entities;
+using DoctorsHub.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoctorsHub.Infrastructure.Repositories
+{
+    public class AppointmentRepository : IAppointmentRepository
+    {
+        //private feilds
+        private readonly ApplicationDbContext _db;
+
+        //constructor
+        public AppointmentRepository(ApplicationDbContext db) 
+        {
+            _db = db;
+        }
+
+        public async Task AddAsync(Appointment appointment)
+        {
+            await _db.AddAsync(appointment);
+
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var appointment = await _db.Appointments.FindAsync(id);
+
+             _db.Appointments.Remove(appointment!);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task<List<Appointment>> GetAppointmentsAsync()
+        {
+            return await _db.Appointments.ToListAsync();
+        }
+
+        public async Task<Appointment> GetByIdAsync(int id)
+        {
+            Appointment? appointment = await _db.Appointments!.FindAsync(id);
+            return appointment!;
+
+        }
+
+        public async Task UpdateAsync(Appointment appointment)
+        {
+            _db.Appointments.Update(appointment);
+            await _db.SaveChangesAsync();
+        }
+    }
+}
