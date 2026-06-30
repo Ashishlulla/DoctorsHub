6d1793c92f2fd7b319575213e20ef14825a67dcc
Ashish Lulla <lullaashish2807@gmail.com>
2026-06-30 23:57:26 +0530

    Implement Appointment DTOs for CRUD operations

---
+++ DoctorHub.Application/DTOs/Appoitments/UpdateAppointmentDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoctorsHub.Application.DTOs.Appoitments
+{
+    internal class UpdateAppointmentDto
+    {
+    }
+}
