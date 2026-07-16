d07f325c003091c679be736a902e37023aef4279
Ashish Lulla <lullaashish2807@gmail.com>
2026-07-16 15:22:36 +0530

    Implement Business Insights time filters and enhance dashboard navigation

---
+++ DoctorsHub.Domain/Enums/AnalyticsTimeFilter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoctorsHub.Domain.Enums
+{
+    public enum AnalyticsTimeFilter
+    {
+        Today,
+        Week, 
+        Month,
+        Quarter,
+        SixMonths,
+        Year
+
+    }
+}
