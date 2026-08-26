696e324e57418b5f017f0296e117a1ab46a0c9f4
Ashish Lulla <lullaashish2807@gmail.com>
2026-08-26 13:19:57 +0530

    Add Department creation page and form styling

--- DoctorsHub/Controllers/DepartmentController.cs
+++ DoctorsHub/Controllers/DepartmentController.cs
@@ -1,8 +1,11 @@
-﻿using DoctorsHub.Web.Services;
+﻿using DoctorsHub.Application.DTOs.Departments;
+using DoctorsHub.Application.DTOs.Doctors;
+using DoctorsHub.Web.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DoctorsHub.Web.Controllers
 {
+    [Route("[controller]")]
     public class DepartmentController : Controller
     {
 
@@ -15,11 +18,39 @@ namespace DoctorsHub.Web.Controllers
             _departmentApiService = departmentApiService;
         }
 
-
+        [HttpGet]
+        [Route("[action]")]
         public async Task<IActionResult> Index()
         {
             var departments = await  _departmentApiService.GetDepartmentsAsync();
             return View(departments);
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public IActionResult Create() 
+        {
+            return View(new CreateDepartmentDto());
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<IActionResult> Create(CreateDepartmentDto createDepartmentDto) 
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(createDepartmentDto);
+            }
+
+            try
+            {
+                await _departmentApiService.CreateDepartmentAsync(createDepartmentDto);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex) 
+            {
+                return View(createDepartmentDto);
+            }
         }
     }
 }
